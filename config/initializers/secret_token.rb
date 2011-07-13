3e39625928804093ed8fa23e0e4f43f24a092435
Mauricio M. de Almeida <mauriciomdea@gmail.com>
2011-07-13 17:15:13 -0300

    Landing page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DesapegarSe::Application.config.secret_token = '593fbfa7e730bed34ce674c513f62c5d52f649c359af2fb02eb59a7b19ed1172d279a9013dbebf453e3179edf270019b797c0d272f31adfa1c4ac7e3dd1ca4fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DesapegarSe::Application.config.session_store :cookie_store, :key => '_desapegar.se_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DesapegarSe::Application.config.session_store :active_record_store
